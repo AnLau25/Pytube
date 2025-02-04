fb866600c5de41d0c3547db7639929f0a00b5060
AnLau25 <avald024@uottawa.ca>
2025-02-03 21:35:55 -0500

    upup

--- down_cli.py
+++ down_cli.py
@@ -11,10 +11,36 @@ def searchResolution():
             for i in video.streams.filter(file_extension='mp4'):
                 rsltns.append(i.resolution)
             vid_rsltns['values']=rsltns
-            showinfo(title='Search Complete', message='Elije de las resoluciones disponobles')
+            showinfo(title='Busqueda Completada', message='Elije de las resoluciones disponobles')
         except:
             showerror(title='Error', message='Error al buscar resoluciones!\n'\
                 'Posibles razones\n->Conexion inestable\n->Link invalido')
+
+def download_vid():
+    try:
+        vid_link = url_entry.get()
+        rsltn = video_res.get()
+        if rsltn=='' and vid_link=='':
+            showerror(title='Error', message='Faltan el link y la resolución')
+        elif rsltn=='':
+            showerror(title='Error', message='Seleccione una resolución')
+        elif rsltn=='None':
+            showerror(title='Error', message='La resolución no puede ser "Nada"')
+        else:
+            try:
+                def on_progress(stream, chunks, bytes_left):
+                    total_size= stream.filesize
+                    
+                    def get_form_size(total_size, factor=1024, suffix='B'):
+                        for unit in ["","K","M","G","T","P","E","Z"]:
+                            if total_size<factor:
+                                return f"{tota_size:.2f}{unit}{suffix}"
+                            tota_size/=factor
+                    return f"{tota_size:.2f}Y{suffix}"
+                formatted_size = get_form_size(total_size)
+                            
+            
+        
 
 # toma el URL del video como argumento
 def vid_down(vid_url):
